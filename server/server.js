da40e3032efd15c1de16a770ac7d9561d94b81c8
SujayHerle <sujayherles@gmail.com>
2017-12-03 13:31:30 +0530

    Add POST /todos

---
+++ server/server.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose.js');
+var {Todo} = require('./models/todo.js');
+var {User} = require('./models/user.js');
+
+var app = express();
+
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req,res) => {
+  var todo = new Todo({
+    text:req.body.text
+  });
+
+  todo.save().then((doc)=>{
+    res.send(doc);
+  },(e)=>{
+    res.status(400).send(e);
+  });
+});
+
+
+
+
+app.listen(3000,() => {
+  console.log("Started on 3000");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var newTodo = new Todo({text:'Cook Dinner'});
+
+newTodo.save().then((doc)=>{
+  console.log('Saved Doc ',doc);
+},(e)=>{
+  console.log('Unable to save ',e);
+});
+var otherTodo = new Todo({text:'Cook Dinner',
+completed:true,
+completedAt:123});
+
+otherTodo.save().then((doc)=>{
+  console.log('Saved Doc ',doc);
+},(e)=>{
+  console.log('Unable to save ',e);
+});
+
+
+
+var user = new User({email:'  abcd@gmail.com    '});
+user.save().then((doc)=>{
+  console.log('Saved Doc ',doc);
+},(e)=>{
+  console.log('Unable to save ',e);
+});*/
