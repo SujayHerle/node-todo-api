3c5d9d4ced941db906e21d05d8404a23428492af
SujayHerle <sujayherles@gmail.com>
2018-02-04 16:39:03 +0530

    Test Delete Todo

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -100,3 +100,37 @@ describe('GET /todos/:id',()=>{
   });
 
 });
+
+describe('DELETE /todos/:id',()=>{
+  it('Delete doc',(done)=>{
+    var hexID= todos[1]._id.toHexString()
+    request(app)
+    .delete(`/todos/${hexID}`)
+    .expect(200)
+    .expect((res)=>{
+      expect(res.body.todos._id).toBe(hexID);
+    })
+    .end((err,res)=>{
+      if(err){
+        return done(err);
+      }
+      Todo.findById(hexID).then((todo)=>{
+        expect(null);
+        done();
+      }).catch((e)=>done(e));
+    });
+  });
+  it('should return 404 if todo not found',(done)=>{
+    request(app)
+    .delete(`/todos/${new ObjectID().toHexString()}`)
+    .expect(404)
+    .end(done);
+  });
+
+  it('should return 404 if todo not found',(done)=>{
+    request(app)
+    .delete(`/todos/${new ObjectID().toHexString()}`)
+    .expect(404)
+    .end(done);
+  });
+});
