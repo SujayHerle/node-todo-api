d171d46460b95c36999771195f70a9d460fb0c8c
SujayHerle <sujayherles@gmail.com>
2018-02-05 23:20:21 +0530

    Patch operatio

--- server/server.js
+++ server/server.js
@@ -80,7 +80,7 @@ app.patch('/todos/:id',(req,res)=>{
     body.completedAt = null;
   }
 
-  Todo.findByIdUpdate(id,{$set:body},{new:true}).then((todo)=>{
+  Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
     if(!todo){
       return res.status(404).send();
     }
