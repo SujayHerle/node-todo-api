07c1ebf8537e0ecf892433200537b56a37e342e9
SujayHerle <sujayherles@gmail.com>
2018-03-03 12:10:54 +0530

    Test POST signup

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,18 +1,19 @@
-
 var {User} = require('./../models/user');
 
-var authenticate = (req,res,next)=>{
-  var token=req.header('x_auth');
-  User.findByToken(token).then((user)=>{
-    if(!user){
-        return Promise.reject();
+var authenticate = (req, res, next) => {
+  var token = req.header('x-auth');
+
+  User.findByToken(token).then((user) => {
+    if (!user) {
+      return Promise.reject();
     }
-    req.user=user;
-    req.token=token;
+
+    req.user = user;
+    req.token = token;
     next();
-  }).catch((e)=>{
-    res.status(401).send(e);
+  }).catch((e) => {
+    res.status(401).send();
   });
 };
 
-module.exports={authenticate};
+module.exports = {authenticate};
--- server/models/user.js
+++ server/models/user.js
@@ -49,25 +49,23 @@ UserSchema.methods.generateAuthToken = function(){
   });
 };
 
-UserSchema.statics.findByToken=function(token){
-  var User=this;
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
   var decoded;
 
-  try{
-    decoded=jwt.verify(token,'abc123');
-  } catch(e){
-    //return new Promise((resolve,reject)=>{
-    //reject();
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
     return Promise.reject();
-}
+  }
 
   return User.findOne({
-    '_id':decoded._id,
-    'tokens.token':token,
-    'tokens.access':'auth'
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
   });
-
 };
+
 
 UserSchema.pre('save',function(next){
   var user=this;
@@ -85,4 +83,4 @@ UserSchema.pre('save',function(next){
 
 var User = mongoose.model('User',UserSchema);
 
-module.exports = {User};
+module.exports = {User}
